328e6231ad3c2c3fe5287c62147aaa5bf1c63d34
Sendoa82 <114100108+SendoaAldama@users.noreply.github.com>
2023-02-15 19:20:02 +0100

    3.0
    Generacion de la tabla
    Personaje en el inicio del tablero y marcas en el principio y final del tablero

--- src/app/vista/login/tablero/tablero.component.ts
+++ src/app/vista/login/tablero/tablero.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tablero',
   templateUrl: './tablero.component.html',
   styleUrls: ['./tablero.component.css']
 })
-export class TableroComponent {
+export class TableroComponent implements OnInit{
+
+    ngOnInit(): void {
+
+        //Variables
+        let tablero = document.createElement("table");
+        tablero.id = "tablero";
+
+        let img = document.createElement("img");    //Imagen del personaje
+        img.id = "moni";
+
+        //Codigo
+        for(let i = 0; i < 10; i++)
+        {
+
+            let tr = document.createElement("tr");
+
+            for(let j = 0; j < 10; j++)
+            {
+
+                let td = document.createElement("td");
+                td.setAttribute("punto", i+"."+j);
+
+                tr.appendChild(td);
+            }
+
+            tablero.appendChild(tr);
+
+        }
+
+        document.getElementById("contenedor-tabla")?.appendChild(tablero);
+
+        document.getElementsByTagName("tr")[0].getElementsByTagName("td")[0].id = "inicio";
+        document.getElementsByTagName("tr")[0].getElementsByTagName("td")[0].appendChild(img);
+
+        document.getElementsByTagName("tr")[9].getElementsByTagName("td")[9].id = "fin";
+
+
+
+    }
 
 }
