ad29cac87ab96ce025d35f84430ab9bfaa987137
Sendoa82 <114100108+SendoaAldama@users.noreply.github.com>
2023-03-07 04:26:49 +0100

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FormsModule } from '@angular/forms';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { provideAuth,getAuth } from '@angular/fire/auth';
     FormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,9 @@ export const environment = {
   firebase: {
     projectId: 'sendoajuego',
     appId: '1:537520607250:web:5b68ffc63995ed452948cb',
+    databaseURL: 'https://sendoajuego-default-rtdb.firebaseio.com',
     storageBucket: 'sendoajuego.appspot.com',
+    locationId: 'europe-west',
     apiKey: 'AIzaSyCv5Ja6c0oE1wDlA1y2Y6bL2n0zduEOQHg',
     authDomain: 'sendoajuego.firebaseapp.com',
     messagingSenderId: '537520607250',
